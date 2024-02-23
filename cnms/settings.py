339a23c182fe762308e340cd2ae4123c81374335
hwoldeamanuel <habtamuwh@gmail.com>
2024-02-23 22:33:50 +0300

    restart commitx2

--- cnms/settings.py
+++ cnms/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-&3ep*035k@21#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
