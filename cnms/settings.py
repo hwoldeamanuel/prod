e398ff74de84dda453061678a8e41c8b972c0331
hwoldeamanuel <habtamuwh@gmail.com>
2024-07-07 23:24:39 +0300

    collect static4

--- cnms/settings.py
+++ cnms/settings.py
@@ -174,6 +174,8 @@ STATIC_URL = '/static/'
 #Location of static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # This production code might break development mode, so we check whether we're in DEBUG mode
 #if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
