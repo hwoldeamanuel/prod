0c79d98b82ee36e44ff9ccbb8a4f2cd155d65448
hwoldeamanuel <habtamuwh@gmail.com>
2024-09-09 23:17:30 +0300

    Add .gitignore file to ignore unnecessary files

--- cnms/custom_azure.py
+++
@@ -1,8 +0,0 @@
-from storages.backends.azure_storage import AzureStorage
-
-class AzureMediaStorage(AzureStorage):
-    account_name = 'paqcnmsblob' # <storage_account_name>
-    account_key = 'DRMNWaWCZPYBKM7UAdCKLpTBqQGEhpb9PmFC2Huciu15tcGEyBdiHfmfozH/hyfyv2AlcOYPdwyo+AStVQrz8g==' # <storage_account_key>
-    azure_container = 'media'
-    expiration_secs = None
-
