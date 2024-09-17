36c8cef17ca8a97805f2ceb1ebe7bc075f556f97
hwoldeamanuel <habtamuwh@gmail.com>
2024-09-18 02:43:39 +0300

    new tyriral14

--- home/views.py
+++ home/views.py
@@ -46,7 +46,7 @@ def home(request):
   total_activity = Activity.objects.count
   total_cn=0
   program_users = Program.objects.annotate(num_user=Count("users_role")).order_by('-num_user')[:12]
-  program_cn = Program.objects.annotate(num_cn=Count("icn__activity") + Count("icn")).filter(num_cn__gte=1).order_by('-num_cn')
+  program_cn = Program.objects.annotate(num_cn=Count("icn__activity",distinct=True) + Count("icn",distinct=True)).filter(num_cn__gte=1).order_by('-num_cn')
   
   for program in program_cn:
      total_cn = program.num_cn + total_cn
